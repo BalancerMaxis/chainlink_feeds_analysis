768af7dbeba4d137a98704a919761d8e5809a0e0
Tritium-VLK <11885129+Tritium-VLK@users.noreply.github.com>
2024-12-08 00:45:56 +0100

    Clean up comments

--- get_feeds.py
+++ get_feeds.py
@@ -3,25 +3,25 @@ from bal_tools import Web3RpcByChain, Web3Rpc
 import json
 import os
 
+ ## Accepts a DRPC API key as an environment variable
+ # open registry.json
+ # open a csv file with headers  chain, rate_provider, feed_address, feed_description
+ # loop through all of the chains
+ # look through all of the rate providers
+ # check if each reate provider has chainlink as one of the warnings
+ # if so, connect to it using web3, call: pricefeed(), which returns an address as a result
+ # connect to the pricefeed contract using the address
+ # call description() which returns a string
+ # write the chain, rate_provider, feed_address, feed_description to the csv file
+ # save and close the csv file
 
 # Define the minimal ABIs as constants
 RATE_PROVIDER_ABI = json.loads('[{"constant":true,"inputs":[],"name":"pricefeed","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
 FEED_ABI = json.loads('[{"constant":true,"inputs":[],"name":"description","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]')
-w3 = Web3RpcByChain(os.environ['DRPC_KEY'])
+w3_by_chain=Web3RpcByChain(os.environ['DRPC_KEY'])
 
 def main():
-    ## Accepts a DRPC API key as an environment variable
-    w3_by_chain=Web3RpcByChain(os.environ['DRPC_KEY'])
-    # open registry.json
-    # open a csv file with headers  chain, rate_provider, feed_address, feed_description
-    # loop through all of the chains
-    # look through all of the rate providers
-    # check if each reate provider has chainlink as one of the warnings
-    # if so, connect to it using web3, call: pricefeed(), which returns an address as a result
-    # connect to the pricefeed contract using the address
-    # call description() which returns a string
-    # write the chain, rate_provider, feed_address, feed_description to the csv file
-    # save and close the csv file
+
 
     with open('registry.json') as f:
         registry = json.load(f) # load the json file
@@ -32,9 +32,9 @@ def main():
         for chain in registry:
             print(chain)
             for rate_provider_address, info in registry[chain].items():
-                ## Handle Chainlink  tagged rate providers
+                ## Only handle Chainlink  tagged rate providers
                 #if 'chainlink' in info['warnings'] or 'legacy' in info['warnings']:
-                if True:
+                if True: # Probe all providers in registry
                     rate_provider_address = Web3.to_checksum_address(rate_provider_address)
                     print(f"{rate_provider_address}({info['warnings']})")
                     w3 = w3_by_chain[chain]
